f677f87c65198436ca90c278ce144e043ff2f391
belino93 <abelmadrid93@gmail.com>
2022-12-08 12:47:26 +0100

    [feat]: Create newGame endpoint

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Log;
 
 class GameController extends Controller
 {
+    // SELECT * FROM games
     public function getGames()
     {
         Log::info('Getting all games');
@@ -21,7 +22,7 @@ class GameController extends Controller
                 'data' => $games,
             ], 200);
         } catch (\Throwable $th) {
-            Log::error($th -> getMessage());
+            Log::error($th->getMessage());
             return response([
                 'success' => false,
                 'message' => 'Retrieve games fails',
@@ -29,24 +30,54 @@ class GameController extends Controller
         }
     }
 
+    // SELECT * FROM games WHERE genre=genre
     public function getGamesByGenre($genre)
     {
         try {
 
             Log::info('Getting games by genre');
             $games = DB::table('games')
-                        ->where('genre', '=', $genre)
-                        ->get();
-                        return response([
-                            'success' => true,
-                            'message' => 'All games by genre retrieved',
-                            'data' => $games,
-                        ], 200);
+                ->where('genre', '=', $genre)
+                ->get();
+            return response([
+                'success' => true,
+                'message' => 'All games by genre retrieved',
+                'data' => $games,
+            ], 200);
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+            return response([
+                'success' => false,
+                'message' => 'Retrieve games by genre fails'
+            ], 400);
+        }
+    }
+
+    // Create new game
+    public function newGame(Request $request)
+    {
+        $title = $request->input('title');
+        $genre = $request->input('genre');
+        $developed = $request->input('developed');
+        try {
+            $newGame = DB::table('games')->insertGetId(
+                [
+                    'title' => $title,
+                    'genre' => $genre,
+                    'developed' => $developed,
+                ]
+            );
+
+            return response([
+                'success' => true,
+                'message' => 'Game created successfully',
+                'data' => $newGame,
+            ], 200);
         } catch (\Throwable $th) {
             Log::error($th -> getMessage());
             return response([
                 'success' => false,
-                'message' => 'Retrieve games by genre fails'
+                'message' => 'New game was failed',
             ], 400);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/games', [GameController::class, 'getGames']);
 
 Route::get('/games/{genre}', [GameController::class, 'getGamesByGenre']);
+
+Route::post('/newGame',[GameController::class, 'newGame']);
+
